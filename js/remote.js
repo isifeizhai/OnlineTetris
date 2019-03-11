941a820566fb5b94342e4d7a3a77997a961d24d0
isifeizhai <deadfathome@gmail.com>
2019-03-11 20:31:02 +0800

    联机版俄罗斯方块

---
+++ js/remote.js
@@ -0,0 +1,68 @@
+var Remote = function(socket) {
+    //游戏对象
+    var game;
+    //开始
+    var start = function(type,dir) {
+        var doms = {
+            gameDiv: document.getElementById('remote_game'),
+            nextDiv: document.getElementById('remote_next'),
+            timeDiv: document.getElementById('remote_time'),
+            scoreDiv: document.getElementById('remote_score'),
+            resultDiv: document.getElementById('remote_gameover')
+        }
+        game = new Game();
+        game.init(doms,type, dir);
+    }
+    //绑定事件
+    var bindEvents = function() {
+        socket.on('init',function(data) {
+            start(data.type, data.dir);
+        });
+
+        socket.on('next',function(data){
+            game.performNext(data.type, data.dir);
+        });
+
+        socket.on('rotate', function(){
+            game.rotate();
+        });
+
+        socket.on('right', function(){
+            game.right();
+        });
+
+        socket.on('left', function(){
+            game.left();
+        });
+
+        socket.on('down', function(){
+            game.down();
+        });
+
+        socket.on('fixed', function(){
+            game.fixed();
+        });
+
+        socket.on('fall', function(){
+            game.fall();
+        });
+
+        socket.on('line', function(data){
+            game.checkClear();
+            game.addScore(data);
+        });
+
+        socket.on('time', function(data) {
+            game.setTime(data);
+        });
+
+        socket.on('lose', function() {
+            game.gameover(false);
+        });
+
+        socket.on('addTailLines', function(data) {
+            game.addTailLines(data);
+        });
+    }
+    bindEvents();
+}
